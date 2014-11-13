a979ab20f5b58ef92dfb2f7533bfefc73b6ffcca
movehoon <movehoon@gmail.com>
2014-11-13 18:31:57 +0900

    Add basic button
    connection
    speak
    behaviour 5 buttons

--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -10,36 +10,81 @@ using LitJson;
 
 public class Program : MonoBehaviour {
 
+	public UIInput ipaddress;
+	public UIInput speakText;
+	int port = 2223;
+
 	static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
 	static private string guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void Connect () {
 		Debug.Log ("Connecting");
-		string jsonString = @"{""Dialog"":""hello"", ""TTS"":""Im nao"", ""Behavior"":""Behavior1""}";
+//		string jsonString = @"{""dialog"":""hello"", ""tts"":""Im nao"", ""behavior"":""Behavior1""}";
+//		Hashtable hashTable = JsonParser._Instance.StringToHashTable (jsonString);
+//		Debug.Log (JsonParser._Instance.HashtableToJsonString(hashTable));
+//
+//		string data = hashTable == null ? "{}" : JsonParser._Instance.HashtableToJsonString (hashTable);
+//
+//		Hashtable header = new Hashtable();
+//		header.Add ("Content-Type", "application/json; charset=utf-8");
+//		header.Add ("Content-Length", data.Length );
+//
+//		WWW www = new WWW (ipaddress.value+":"+port.ToString(), Encoding.UTF8.GetBytes(data), header);
+//		if (www.error != null)
+//			Debug.Log ("Error");
+//		else
+//			Debug.Log ("Success");
+	}
+
+	public void Speak () {
+		string jsonString = @"{""dialog"":""";
+		jsonString += speakText.value;
+		jsonString += @"""}";
+		Debug.Log (jsonString);
+		Send (jsonString);
+	}
+
+	public void Behavior1 () {
+		Behavior ("behavior1");
+	}
+	
+	public void Behavior2 () {
+		Behavior ("behavior2");
+	}
+	
+	public void Behavior3 () {
+		Behavior ("behavior3");
+	}
+	
+	public void Behavior4 () {
+		Behavior ("behavior4");
+	}
+	
+	public void Behavior5 () {
+		Behavior ("behavior5");
+	}
+	
+	void Behavior (string str) {
+		string jsonString = @"{""behavior"":""";
+		jsonString += str;
+		jsonString += @"""}";
+		Send (jsonString);
+	}
+
+	void Send (string jsonString) {
 		Hashtable hashTable = JsonParser._Instance.StringToHashTable (jsonString);
 		Debug.Log (JsonParser._Instance.HashtableToJsonString(hashTable));
-
+		
 		string data = hashTable == null ? "{}" : JsonParser._Instance.HashtableToJsonString (hashTable);
-
+		
 		Hashtable header = new Hashtable();
 		header.Add ("Content-Type", "application/json; charset=utf-8");
 		header.Add ("Content-Length", data.Length );
-
-		WWW www = new WWW ("192.168.1.12:2223", Encoding.UTF8.GetBytes(data), header);
+		
+		WWW www = new WWW (ipaddress.value+":"+port.ToString(), Encoding.UTF8.GetBytes(data), header);
 		if (www.error != null)
 			Debug.Log ("Error");
 		else
 			Debug.Log ("Success");
 	}
-
 }
