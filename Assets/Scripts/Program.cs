e74dff808c41f3f904f3e4751475bb0fe4491dfe
Donghoon Lee <info@makesense.cc>
2014-11-14 11:26:33 +0900

    Save ipAddress

--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -10,13 +10,19 @@ using LitJson;
 
 public class Program : MonoBehaviour {
 
+	public const string Key_IpAddr = "Key_IpAddr";
+
 	public UIInput ipaddress;
 	public UIInput speakText;
 	int port = 2223;
 
 	static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
 	static private string guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-	
+
+	void Start () {
+		ipaddress.value = GetIpAddr ();
+	}
+
 	public void Connect () {
 		Debug.Log ("Connecting");
 //		string jsonString = @"{""dialog"":""hello"", ""tts"":""Im nao"", ""behavior"":""Behavior1""}";
@@ -80,11 +86,25 @@ public class Program : MonoBehaviour {
 		Hashtable header = new Hashtable();
 		header.Add ("Content-Type", "application/json; charset=utf-8");
 		header.Add ("Content-Length", data.Length );
-		
+
+		SetIpAddr(ipaddress.value);
 		WWW www = new WWW (ipaddress.value+":"+port.ToString(), Encoding.UTF8.GetBytes(data), header);
 		if (www.error != null)
 			Debug.Log ("Error");
 		else
 			Debug.Log ("Success");
+	}
+
+	static public string GetIpAddr()
+	{
+		if (PlayerPrefs.HasKey(Key_IpAddr) == false)
+		{
+			return "192.168.0.2";
+		}
+		return PlayerPrefs.GetString(Key_IpAddr);
+	}
+	static public void SetIpAddr(string ipAddr)
+	{
+		PlayerPrefs.SetString(Key_IpAddr, ipAddr);
 	}
 }
