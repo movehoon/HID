e0d08f3ef5eeda3136b3fddcc94644dbed8df310
Donghoon Lee <info@makesense.cc>
2015-01-29 14:32:40 +0900

    modify some

--- Assets/Scripts/ImgManager.cs
+++ Assets/Scripts/ImgManager.cs
@@ -11,25 +11,23 @@ public class ImgManager : MonoBehaviour {
 
 	public void ShowImage0 ()
 	{
-		UITexture tex = texture.GetComponentInChildren<UITexture> ();
-		tex.mainTexture = textures[0];
+		SetImage (null);
 	}
 
 	public void ShowImage1 ()
 	{
-		UITexture tex = texture.GetComponentInChildren<UITexture> ();
-		tex.mainTexture = textures[1];
+		SetImage (textures[1]);
 	}
 
 	public void ShowImage2 ()
 	{
-		UITexture tex = texture.GetComponentInChildren<UITexture> ();
-		tex.mainTexture = textures[2];
+		SetImage (textures[2]);
 	}
 
-	public void SetImage(int n)
+	public void SetImage(Texture tex)
 	{
-
+		UITexture tx = texture.GetComponentInChildren<UITexture> ();
+		tx.mainTexture = tex;
 	}
 
 	void Awake ()
